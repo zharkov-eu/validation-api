30901fbb3480bdd17f59ab51b7972164d100e295
Evgeni Zharkov <zharkov.e.u@gmail.com>
2018-05-11 11:39:22 +0300

    Обновление definitions

--- index.d.ts
+++ index.d.ts
@@ -7,19 +7,49 @@ import * as api from "./src/decorator/api";
 import * as shared from "./src/decorator/shared";
 import * as error from "./src/error";
 
-export class ValidationError extends error.ValidationError {}
-export interface IValidationErrorCause extends error.IValidationErrorCause {}
+interface IClassDecoratorOption extends shared.IClassDecoratorOption {
+  throwable?: boolean;
+}
+
+interface IPropDecoratorOption extends api.IPropDecoratorOption {
+  message?: string;
+  required?: boolean;
+}
+
+interface INumberPropDecorationOption extends IPropDecoratorOption, api.INumberPropDecorationOption {
+  min?: number;
+  max?: number;
+}
+
+interface IMemberOfPropDecorationOption extends IPropDecoratorOption, api.IMemberOfPropDecorationOption {
+  array: any[];
+}
+
+export class ValidationError extends error.ValidationError {
+  public constraint: string;
+  public message: string;
+  public property: string;
+  public value: any;
+}
+
+export interface IValidationErrorCause extends error.IValidationErrorCause {
+  constraint: string;
+  message: string;
+  property: string;
+  value: any;
+}
 
 export declare class ValidationDomain {
   __validationError: () => IValidationErrorCause[] | undefined;
 }
-export declare const Validate: typeof shared.Validate;
-export declare const NotEmpty: typeof api.NotEmpty;
-export declare const IsBoolean: typeof api.IsBoolean;
-export declare const IsNumber: typeof api.IsNumber;
-export declare const IsPositiveNumber: typeof api.IsPositiveNumber;
-export declare const IsPositiveOrZeroNumber: typeof api.IsPositiveOrZeroNumber;
-export declare const NotEmptyString: typeof api.NotEmptyString;
-export declare const IsEmail: typeof api.IsEmail;
-export declare const IsPhone: typeof api.IsPhone;
-export declare const IsMemberOf: typeof api.IsMemberOf;
+
+export declare const Validate: (option?: IClassDecoratorOption) => <T extends { new(...args: any[]): {} }>(target: T) => any;
+export declare const NotEmpty: (option?: IPropDecoratorOption) => any;
+export declare const IsBoolean: (option?: IPropDecoratorOption) => any;
+export declare const IsNumber: (option?: INumberPropDecorationOption) => any;
+export declare const IsPositiveNumber: (option?: INumberPropDecorationOption) => any;
+export declare const IsPositiveOrZeroNumber: (option?: INumberPropDecorationOption) => any;
+export declare const NotEmptyString: (option?: IPropDecoratorOption) => any;
+export declare const IsEmail: (option?: IPropDecoratorOption) => any;
+export declare const IsPhone: (option?: IPropDecoratorOption) => any;
+export declare const IsMemberOf: (option?: IMemberOfPropDecorationOption) => any;
