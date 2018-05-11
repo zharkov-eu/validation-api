11e1d84098f055ea73c53c5b458398e03958ec81
Evgeni Zharkov <zharkov.e.u@gmail.com>
2018-05-11 12:16:52 +0300

    Добавлен index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+/*
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * @author Evgeni Zharkov <zharkov.ev.u@yandex.ru>
+ */
+
+"use strict";
+
+const api = require("./src/decorator/api");
+const shared = require("./src/decorator/shared");
+const error = require("./src/error");
+
+exports.ValidationError = error.ValidationError;
+
+function ValidationDomain() {
+    this.__validationError = function () {
+        return undefined;
+    };
+    return this;
+}
+
+exports.ValidationDomain = ValidationDomain;
+
+exports.Validate = shared.Validate;
+exports.NotEmpty = api.NotEmpty;
+exports.IsBoolean = api.IsBoolean;
+exports.IsNumber = api.IsNumber;
+exports.IsPositiveNumber = api.IsPositiveNumber;
+exports.IsPositiveOrZeroNumber = api.IsPositiveOrZeroNumber;
+exports.NotEmptyString = api.NotEmptyString;
+exports.IsEmail = api.IsEmail;
+exports.IsPhone = api.IsPhone;
+exports.IsMemberOf = api.IsMemberOf;
