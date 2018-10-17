c66416c7db1098b6df972d0ef6f21df14af8ce66
Evgeni Zharkov <zharkov.ev.u@yandex.ru>
2018-10-17 18:20:09 +0300

    Добавлены определния для __validation

--- src/abstract.d.ts
+++ src/abstract.d.ts
@@ -1,3 +1,5 @@
+import { IValidationErrorCause } from "./error";
 export declare abstract class AbstractValidated {
+    __validationError: () => IValidationErrorCause[];
     protected constructor(entity: any);
 }
